fa989c463926955a8780c8597533e7155bdce304
Nibia <nibiabotsi@gmail.com>
2024-08-30 20:14:01 -0400

    data

--- lightsail/main.tf
+++ lightsail/main.tf
@@ -14,11 +14,20 @@ provider "aws" {
 resource "aws_lightsail_instance" "server" {
   name = "webserver"
   availability_zone = "us-east-1a"
-  blueprint_id = "debian_12"
+  blueprint_id = "amazon_linux_2"
   bundle_id = "nano_3_0"
-  key_pair_name = "haproxy"
+  key_pair_name = "week2"
+  user_data = "sudo yum install -y httpd && sudo systemctl start httpd && sudo systemctl enable httpd && echo '<h1>welcome to manager class</h1>' | sudo tee /var/www/html/index.html"
   tags = {
     env = "revision"
   }
+  
+}
+output "public_ip" {
+  value = aws_lightsail_instance.server.public_ip_address
+  
+}
+output "private_ip" {
+  value = aws_lightsail_instance.server.private_ip_address
   
 }
